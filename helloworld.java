b1eef23c60e484a0ed9b7f5871b18b7ebda6f46c
DebendraSahu <92933669+DebendraSahu@users.noreply.github.com>
2022-08-02 18:36:11 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+
+//Program to print hello world using java
+
+public class helloworld {  //class name
+    public static void main(String[] args) {
+        System.out.println("hello  world"); // it will print hello world
+    }
+}
